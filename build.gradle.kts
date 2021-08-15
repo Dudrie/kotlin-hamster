772a2d3963e0048208d59a10cf94f6daeae52df7
Dudrie <Dudrie@gmx.de>
2021-08-15 15:44:44 +0200

    Add maven-publish gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val artifactPrefix = rootProject.name
 group = "de.github.dudrie"
 version = "1.0"
 
@@ -8,19 +9,34 @@ plugins {
 
     id("org.jetbrains.compose") version "1.0.0-alpha1"
     id("org.jetbrains.dokka") version "1.5.0"
+    id("maven-publish")
 }
 
 allprojects {
     group = "de.github.dudrie"
     version = "0.1"
 
+    plugins.apply("maven-publish")
     plugins.apply("org.jetbrains.dokka")
 
     repositories {
         google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         mavenCentral()
+    }
 
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("maven") {
+                    groupId = group.toString()
+                    artifactId = "$artifactPrefix-${project.name}"
+                    version = version
+
+                    from(components["java"])
+                }
+            }
+        }
     }
 
     tasks.withType<KotlinCompile> {
