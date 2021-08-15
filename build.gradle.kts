8a9afd2a88426d7e9475d81e456550ef895530e4
Dudrie <Dudrie@gmx.de>
2021-08-15 14:43:07 +0200

    Add Dokka gradle plugin to generate docs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,14 @@ plugins {
     kotlin("jvm") version "1.5.21"
 
     id("org.jetbrains.compose") version "1.0.0-alpha1"
+    id("org.jetbrains.dokka") version "1.5.0"
 }
 
 allprojects {
     group = "de.github.dudrie"
     version = "0.1"
+
+    plugins.apply("org.jetbrains.dokka")
 
     repositories {
         google()
@@ -23,4 +26,8 @@ allprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }
+}
+
+tasks.dokkaHtmlMultiModule.configure {
+    outputDirectory.set(buildDir.resolve("kdoc"))
 }
