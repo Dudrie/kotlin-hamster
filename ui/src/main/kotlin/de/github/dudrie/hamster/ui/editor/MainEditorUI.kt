69423d2de5c92defd53519fc43bae34c47630531
Dudrie <Dudrie@gmx.de>
2021-08-16 21:13:51 +0200

    Add logic to open a dummy edit dialog if one clicks on a tile.

--- ui/src/main/kotlin/de/github/dudrie/hamster/ui/editor/MainEditorUI.kt
+++ ui/src/main/kotlin/de/github/dudrie/hamster/ui/editor/MainEditorUI.kt
@@ -1,25 +1,25 @@
 package de.github.dudrie.hamster.ui.editor
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import de.github.dudrie.hamster.datatypes.Location
 import de.github.dudrie.hamster.de.github.dudrie.hamster.interfaces.AbstractEditableTerritory
 import de.github.dudrie.hamster.ui.components.board.BoardGrid
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -50,63 +50,81 @@ fun BoardForEditor(territory: AbstractEditableTerritory, modifier: Modifier = Mo
         modifier = modifier.padding(16.dp).widthIn(min = minWidth.dp, max = maxWidth.dp),
         contentAlignment = Alignment.Center
     ) {
-        var popupState by mutableStateOf<PopupState?>(null)
-
         BoardGrid(territory, 1.dp) { location, tileModifier ->
+            var editedTile by EditorState.editedTile
             val interactionSource = remember { MutableInteractionSource() }
 
             LaunchedEffect(interactionSource) {
                 interactionSource.interactions.collect {
-                    if (it is PressInteraction.Press) {
-                        if (popupState != null && popupState!!.location != location) {
-                            popupState = null
-                        }
-                    }
-
                     if (it is PressInteraction.Release) {
-                        popupState = PopupState(location, it.press.pressPosition)
+                        launch {
+                            editedTile = EditedTile(location)
+                        }
                     }
                 }
             }
 
-            Box(
+            Column(
                 tileModifier.clickable(
                     interactionSource = interactionSource,
                     indication = LocalIndication.current,
-                    enabled = popupState?.location != location
+                    enabled = editedTile?.location != location
                 ) { }
             ) {
-                popupState?.let {
-                    if (it.location == location) {
-                        val (x, y) = it.offset
-                        Popup(
-                            offset = IntOffset(x.toInt(), y.toInt()),
-                        ) {
-                            Surface(
-                                Modifier.size(150.dp, 100.dp),
-                                elevation = 16.dp,
-                                shape = RoundedCornerShape(8.dp),
-                            ) {
-                                Box(Modifier.padding(8.dp).background(Color.Red)) {
-                                    Text("POPUP!!!")
-                                }
-                            }
-                        }
-                    }
-                }
-
                 Text("$location")
+
+                if (editedTile?.location == location) {
+                    Text("GETS EDITED")
+                }
             }
         }
     }
 }
 
-data class PopupState(val location: Location, val offset: Offset)
+class EditedTile(val location: Location)
 
+object EditorState {
+    val editedTile = mutableStateOf<EditedTile?>(null)
+}
+
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Toolbox(modifier: Modifier = Modifier) {
     Box(modifier) {
-        Text("TOOLBOX")
-    }
+        val editedTile by EditorState.editedTile
+        val drawerState = remember { MutableTransitionState(false) }
 
+        LaunchedEffect(editedTile) {
+            if (drawerState.currentState) {
+                delay(200L)
+            }
+            drawerState.targetState = editedTile != null
+        }
+
+        Column(Modifier.background(Color.Yellow).fillMaxSize()) {
+            Text("TOOLBOX")
+        }
+
+        AnimatedVisibility(
+            visibleState = drawerState,
+            enter = fadeIn() + slideInHorizontally({ it / 2 }),
+            exit = fadeOut() + slideOutHorizontally({ it / 2 })
+        ) {
+            val scope = rememberCoroutineScope()
+            Column(Modifier.fillMaxSize().background(Color.Cyan)) {
+                if (editedTile != null) {
+                    Text("IM EDITING ${editedTile!!.location}")
+                }
+
+                Button(onClick = {
+                    scope.launch {
+                        drawerState.targetState = false
+                        EditorState.editedTile.value = null
+                    }
+                }) {
+                    Text("CLOSE")
+                }
+            }
+        }
+    }
 }
