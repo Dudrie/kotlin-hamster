a089ab6807292445158bbfcd17c20ba26b288e61
Dudrie <Dudrie@gmx.de>
2021-08-16 16:35:30 +0200

    Add EditorWindow class with empty content function.
    This serves as a reminder that the editor still has to be implemented.

---
+++ ui/src/main/kotlin/de/github/dudrie/hamster/ui/application/windows/EditorWindow.kt
@@ -0,0 +1,18 @@
+package de.github.dudrie.hamster.ui.application.windows
+
+import androidx.compose.runtime.Composable
+import de.github.dudrie.hamster.ResString
+
+/**
+ * Window showing the editor's UI.
+ */
+class EditorWindow() : ApplicationWindow(ResString.get("window.editor.title")) {
+
+    /**
+     * Renders the UI for the editor.
+     */
+    @Composable
+    override fun content() {
+        TODO("Not yet implemented")
+    }
+}
