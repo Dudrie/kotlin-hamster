8ea5ff13d4dff5b7b576c505045273dc789b1f1a
Dudrie <Dudrie@gmx.de>
2021-08-17 17:25:00 +0200

    Add editor start to Main file in :main.

--- main/src/main/kotlin/Main.kt
+++ main/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
+import de.github.dudrie.hamster.datatypes.Size
+import de.github.dudrie.hamster.external.model.EditableTerritory
 import de.github.dudrie.hamster.external.model.HamsterGame
+import de.github.dudrie.hamster.ui.application.windows.EditorWindow
+import java.util.concurrent.CountDownLatch
 
 private fun runSampleGame(game: HamsterGame) {
+    game.gameCommands.setSpeed(7f)
     val paule = game.paule
 
     repeat(3) {
@@ -8,7 +13,9 @@ private fun runSampleGame(game: HamsterGame) {
     }
     paule.move()
 
-    paule.talk("I said something :)")
+    repeat(25) {
+        paule.talk("I said something :) #$it")
+    }
 
     repeat(3) {
         paule.turnLeft()
@@ -16,15 +23,24 @@ private fun runSampleGame(game: HamsterGame) {
     repeat(2) {
         paule.move()
     }
+
     repeat(2) {
         paule.dropGrain()
     }
     paule.move()
 }
 
-internal fun main() {
-    val game = HamsterGame("/territories/testTer01.json")
-    game.startGame()
+internal fun startEditor() {
+    EditorWindow(EditableTerritory(Size(5, 3))).show(
+        CountDownLatch((1))
+    )
+}
 
-    runSampleGame(game)
+internal fun main() {
+    startEditor()
+
+//    val game = HamsterGame("/territories/testTer01.json")
+//    game.startGame(false)
+//
+//    runSampleGame(game)
 }
