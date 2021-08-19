41e3c0d11808031597af0e5fe4c82d6da4e49b22
Dudrie <Dudrie@gmx.de>
2021-08-19 15:13:22 +0200

    Fix reload of BoardOfEditor requiring a flickering timeout.

--- editor/src/main/kotlin/de/github/dudrie/hamster/editor/MainEditorUI.kt
+++ editor/src/main/kotlin/de/github/dudrie/hamster/editor/MainEditorUI.kt
@@ -1,14 +1,16 @@
 package de.github.dudrie.hamster.editor
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import de.github.dudrie.hamster.editor.dialog.DialogManager
-import kotlinx.coroutines.delay
 
 /**
  * Main composable for the editor UI.
@@ -18,20 +20,9 @@ fun MainEditorUI() {
     Scaffold(topBar = { EditorAppBar() }) {
         Row(modifier = Modifier.fillMaxSize()) {
             val territory by EditorState.territory
-            var showBoard by remember { mutableStateOf(false) }
 
-            LaunchedEffect(territory) {
-                // Make sure the old board gets "cleaned" end properly recreated.
-                // This makes all internal state subscriptions invalid.
-                showBoard = false
-                delay(250L)
-                showBoard = true
-            }
-
-            if (showBoard) {
+            key(territory) {
                 BoardForEditor(territory, Modifier.weight(1f).fillMaxHeight())
-            } else {
-                Spacer(Modifier.weight(1f).background(Color.Red))
             }
 
             EditorSidepanel(Modifier.fillMaxHeight().width(300.dp))
