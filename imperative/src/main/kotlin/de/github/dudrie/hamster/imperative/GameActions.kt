969d073cbbde0a05bf170f1f10a916fc27475cce
Dudrie <Dudrie@gmx.de>
2021-08-20 15:44:44 +0200

    Add documentation for the imperative functions.

--- imperative/src/main/kotlin/de/github/dudrie/hamster/imperative/GameActions.kt
+++ imperative/src/main/kotlin/de/github/dudrie/hamster/imperative/GameActions.kt
@@ -2,14 +2,34 @@ package de.github.dudrie.hamster.imperative
 
 import de.github.dudrie.hamster.external.model.HamsterGame
 
+/**
+ * The game used by the imperative commands.
+ *
+ * If no game is started it is `null`.
+ */
 internal var imperativeGlobalGame: HamsterGame? = null
+
+/**
+ * Starts a new hamster game with the given [territoryFile].
+ *
+ * After loading and starting the game the global [imperativeGlobalGame] is set to this game.
+ *
+ * There can only be one game started at any point in time.
+ *
+ * @param territoryFile File to load the territory from. Has to be in the resources/ folder. If not provided a default territory gets loaded.
+ */
 fun startGame(territoryFile: String? = null) {
+    require(imperativeGlobalGame == null) { "There can only be one game used at a time." }
     val game = HamsterGame(territoryFile)
     game.startGame(true)
     imperativeGlobalGame = game
 }
 
+/**
+ * Stops the current game (if started and present).
+ *
+ * It does **NOT** change the [imperativeGlobalGame].
+ */
 fun stopGame() {
     imperativeGlobalGame?.stopGame()
-    imperativeGlobalGame = null
 }
--- imperative/src/main/kotlin/de/github/dudrie/hamster/imperative/HamsterActions.kt
+++ imperative/src/main/kotlin/de/github/dudrie/hamster/imperative/HamsterActions.kt
@@ -1,40 +1,80 @@
 package de.github.dudrie.hamster.imperative
 
+/**
+ * Moves the hamster one tile in the direction it faces.
+ *
+ * The destination tile must not be blocked.
+ *
+ * @see canYouMove
+ */
 fun move() {
     isGameStarted()
     imperativeGlobalGame?.paule?.move()
 }
 
+/**
+ * Turns the hamster 90 degrees counterclockwise.
+ */
 fun turnLeft() {
     isGameStarted()
     imperativeGlobalGame?.paule?.turnLeft()
 }
 
+/**
+ * The hamster picks up a grain from the tile beneath it.
+ *
+ * The tile must have at least one grain on it.
+ */
 fun pickGrain() {
     isGameStarted()
     imperativeGlobalGame?.paule?.pickGrain()
 }
 
+/**
+ * The hamster drops a grain on the tile beneath it.
+ *
+ * The hamster must have at least one grain in its mouth.
+ *
+ * @see isYourMouthEmpty
+ */
 fun dropGrain() {
     isGameStarted()
     imperativeGlobalGame?.paule?.dropGrain()
 }
 
-fun canYouMove() {
+/**
+ * Can the hamster move one tile in its facing direction?
+ *
+ * If yes this function returns `true`, otherwise `false.
+ */
+fun canYouMove(): Boolean {
     isGameStarted()
-    imperativeGlobalGame?.paule?.canYouMove()
+    return imperativeGlobalGame?.paule?.canYouMove() ?: false
 }
 
-fun isYourMouthEmpty() {
+/**
+ * Has the hamster at least one grain in its mouth?
+ *
+ * If yes this function returns `true`, otherwise `false`.
+ */
+fun isYourMouthEmpty(): Boolean {
     isGameStarted()
-    imperativeGlobalGame?.paule?.isYourMouthEmpty()
+    return imperativeGlobalGame?.paule?.isYourMouthEmpty() ?: false
 }
 
-fun hasYourTileAGrain() {
+/**
+ * Has the tile on which the hamster sits at least one grain on it?
+ *
+ * If yes this function returns `true`, otherwise `false.
+ */
+fun hasYourTileAGrain(): Boolean {
     isGameStarted()
-    imperativeGlobalGame?.paule?.hasYourTileAGrain()
+    return imperativeGlobalGame?.paule?.hasYourTileAGrain() ?: false
 }
 
+/**
+ * The hamster "says" the given [message] printing it to the game log.
+ */
 fun say(message: String) {
     isGameStarted()
     imperativeGlobalGame?.paule?.say(message)
