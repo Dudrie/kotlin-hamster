7740f17f29d18ce879ced4b5985a5f0bcad505fd
Dudrie <Dudrie@gmx.de>
2021-08-31 23:41:11 +0200

    Add settings to publish artifacts to the GitHub Package Repository.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 import org.jetbrains.dokka.gradle.DokkaTaskPartial
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
 
 val artifactPrefix = rootProject.name
 group = "de.github.dudrie"
 version = "0.1"
+
+val properties = Properties()
+properties.load(project.file("local.properties").inputStream())
+val gprUser = properties["gpr.user"] as String?
+val gprToken = properties["gpr.token"] as String?
 
 plugins {
     kotlin("jvm") version "1.5.21"
@@ -25,6 +31,12 @@ allprojects {
         google()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         mavenCentral()
+    }
+
+    afterEvaluate {
+        java {
+            withJavadocJar()
+        }
     }
 
     tasks.withType<KotlinCompile> {
@@ -51,6 +63,17 @@ subprojects {
 
                 afterEvaluate {
                     from(components["java"])
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/Dudrie/kotlin-hamster")
+                credentials {
+                    username = gprUser
+                    password = gprToken
                 }
             }
         }
