47c457fb7ffa2f279229291ad815febda5145670
Dudrie <Dudrie@gmx.de>
2021-09-01 12:35:04 +0200

    Add missing documentation.

--- ui/src/main/kotlin/de/github/dudrie/hamster/ui/helpers/ResourceHelpers.kt
+++ ui/src/main/kotlin/de/github/dudrie/hamster/ui/helpers/ResourceHelpers.kt
@@ -3,6 +3,9 @@ package de.github.dudrie.hamster.ui.helpers
 import de.github.dudrie.hamster.internal.model.territory.GameTileType
 import de.github.dudrie.hamster.ui.R
 
+/**
+ * Returns the path to the resource associated with this [GameTileType].
+ */
 fun GameTileType.getResource() = when (this) {
     GameTileType.Wall -> R.images.wall
     GameTileType.Floor -> R.images.floor
