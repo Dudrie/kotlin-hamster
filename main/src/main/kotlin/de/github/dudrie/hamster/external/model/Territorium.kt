496a902dc8fbf196dbce6515283d9d312aa2d5ad
Dudrie <Dudrie@gmx.de>
2023-02-27 17:34:19 +0100

    Add holeKornAnzahlBei() function.

--- main/src/main/kotlin/de/github/dudrie/hamster/external/model/Territorium.kt
+++ main/src/main/kotlin/de/github/dudrie/hamster/external/model/Territorium.kt
@@ -7,29 +7,44 @@ import de.github.dudrie.hamster.internal.model.territory.GameTerritory
 import de.github.dudrie.hamster.internal.model.territory.GameTile
 
 /**
- * Territory used in the [HamsterSpiel].
+ * Territorium, welches im [HamsterSpiel] benutzt wird.
  *
- * @param spiel Hamster game which uses this [Territorium].
- * @param internalTerritory Reference to the internal [GameTerritory].
+ * @param spiel [HamsterSpiel], welches das Territorium benutzt [Territorium].
+ * @param internalTerritory Referenz auf das interne [GameTerritory].
  */
 class Territorium(val spiel: HamsterSpiel, internal val internalTerritory: GameTerritory) : AbstraktesTerritorium() {
 
     /**
-     * [Size] of this territory.
+     * [Size] des Territoriums.
      */
     override val abmessungen: Size = internalTerritory.size
 
     /**
-     * Meters a single tile in this territory represents.
+     * Meter, denen ein einzelnes Feld in diesem Territorium entspricht.
      */
     override val feldZuMeterSkalierung: Double
         get() = internalTerritory.tileToMeterScaling
 
     /**
-     * Returns the [GameTile] at the [ort].
+     * Gibt das [GameTile] am übergebenen [ort] zurück.
      *
-     * The [ort] must be inside this territory.
+     * Der [ort] muss sich innerhalb dieses Territoriums befinden.
+     *
+     * @return [GameTile] am gegebenen [ort].
+     *
+     * @throws IllegalArgumentException Wenn der [ort] außerhalb des Territoriums liegt.
      */
     override fun holeFeldBei(ort: SpielOrt): GameTile = internalTerritory.getTileAt(ort)
 
+    /**
+     * Gibt die Anzahl Körner am übergebenen [ort] zurück.
+     *
+     * Der [ort] muss sich innerhalb dieses Territoriums befinden.
+     *
+     * @return Anzahl Körner am gegebenen [ort].
+     *
+     * @throws IllegalArgumentException Wenn der [ort] außerhalb des Territoriums liegt.
+     */
+    fun holeKornAnzahlBei(ort: SpielOrt): Int = internalTerritory.getTileAt(ort).grainCount
+    
 }
