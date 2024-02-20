ec3ce72a83b83c7cd4b97f813cf35df849b282d0
Dudrie <Dudrie@gmx.de>
2024-02-21 00:03:12 +0100

    Fix wrong parameter.

--- editor/src/main/kotlin/Main.kt
+++ editor/src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 import de.github.dudrie.hamster.editor.application.EditorWindow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import java.util.concurrent.CountDownLatch
 
 /**
  * Start the editor in a new window with a default territory size.
  */
 fun main() {
     runBlocking {
-        EditorWindow().show(CountDownLatch(1))
+        EditorWindow().show()
 
         delay(2000)
     }
