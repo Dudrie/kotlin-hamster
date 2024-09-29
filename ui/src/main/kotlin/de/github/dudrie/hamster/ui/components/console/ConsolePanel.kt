c6ee3c64b64203b02e37b5f11ea0755b6c6840f2
Dudrie <Dudrie@gmx.de>
2024-09-29 20:48:40 +0200

    Add animations for console messages.

--- ui/src/main/kotlin/de/github/dudrie/hamster/ui/components/console/ConsolePanel.kt
+++ ui/src/main/kotlin/de/github/dudrie/hamster/ui/components/console/ConsolePanel.kt
@@ -1,32 +1,36 @@
 package de.github.dudrie.hamster.ui.components.console
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import de.github.dudrie.hamster.ui.extensions.getText
 import de.github.dudrie.hamster.ui.generated.Res
 import de.github.dudrie.hamster.ui.generated.console_kommando_nummer
 import de.github.dudrie.hamster.ui.model.UIViewModel
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun ConsolePanel(modifier: Modifier = Modifier, viewModel: UIViewModel = viewModel()) {
-    val nachrichten by viewModel.spielLog.nachrichten.collectAsState()
+    val messages by viewModel.spielLog.nachrichten.collectAsState()
+    val showUntilNumber by viewModel.spielLog.zeigeBisIndex.collectAsState()
     val scrollState = rememberScrollState()
 
     Box(modifier = modifier) {
@@ -36,28 +40,45 @@ fun ConsolePanel(modifier: Modifier = Modifier, viewModel: UIViewModel = viewMod
         ) {
             Spacer(Modifier.height(0.dp))
 
-            nachrichten.forEachIndexed { index, nachricht ->
-                Card(Modifier.fillMaxWidth()) {
-                    Text(
-                        text = stringResource(
-                            Res.string.console_kommando_nummer,
-                            nachrichten.size - index
-                        ),
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(
-                            start = 8.dp,
-                            end = 8.dp,
-                            top = 6.dp,
-                            bottom = 2.dp
-                        )
-                    )
+            messages.forEachIndexed { index, message ->
+                key(message) {
+                    val messageNumber = messages.size - index
+                    var visible by remember { mutableStateOf(false) }
 
-                    HorizontalDivider()
+                    LaunchedEffect(showUntilNumber) {
+                        visible = messageNumber <= showUntilNumber + 1
+                    }
 
-                    Text(
-                        text = nachricht.getText(),
-                        modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)
-                    )
+                    AnimatedVisibility(
+                        visible = visible,
+                        enter = slideIn { IntOffset(it.width, 0) } + fadeIn(),
+                        exit = slideOut { IntOffset(it.width, 0) } + fadeOut(),
+                        label = "Message$messageNumber",
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Card(Modifier.fillMaxWidth()) {
+                            Text(
+                                text = stringResource(
+                                    Res.string.console_kommando_nummer,
+                                    messageNumber
+                                ),
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(
+                                    start = 8.dp,
+                                    end = 8.dp,
+                                    top = 6.dp,
+                                    bottom = 2.dp
+                                )
+                            )
+
+                            HorizontalDivider()
+
+                            Text(
+                                text = message.getText(),
+                                modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
