9ef0c324007bbc019904a0d8f14560c4cc2fc1b8
Dudrie <dudrie@gmx.de>
2024-09-30 06:40:26 +0200

    Add comments for hamster commands.

--- functional/src/main/kotlin/de/github/dudrie/hamster/functional/HamsterAktionen.kt
+++ functional/src/main/kotlin/de/github/dudrie/hamster/functional/HamsterAktionen.kt
@@ -1,27 +1,65 @@
+@file:Suppress("unused")
+
 package de.github.dudrie.hamster.functional
 
+/**
+ * Lässt den Hamster ein Feld in Blickrichtung laufen.
+ *
+ * Das Zielfeld darf nicht blockiert sein.
+ *
+ * @see istVorDirFrei
+ */
 fun laufe() {
     SingletonSpiel.paule.laufe()
 }
 
+/**
+ * Dreht den Hamster 90° gegen den Uhrzeigersinn.
+ */
 fun dreheNachLinks() {
     SingletonSpiel.paule.dreheNachLinks()
 }
 
+/**
+ * Der Hamster sammelt ein Korn von seinem Feld auf.
+ *
+ * Auf dem Feld muss mindestens ein Korn liegen.
+ */
 fun sammleKornAuf() {
     SingletonSpiel.paule.sammleKornAuf()
 }
 
+/**
+ * Der Hamster legt ein Korn auf sein Feld ab.
+ *
+ * Er muss mindestens ein Korn im Mund haben.
+ *
+ * @see istDeinMundLeer
+ */
 fun legeKornAb() {
     SingletonSpiel.paule.legeKornAb()
 }
 
+/**
+ * Kann der Hamster sich auf das Feld direkt vor ihm bewegen?
+ */
 fun istVorDirFrei(): Boolean = SingletonSpiel.paule.istVorDirFrei()
 
+/**
+ * Liegt auf dem Feld des Hamsters mindestens ein Korn?
+ */
 fun liegtEinKornAufDeinemFeld(): Boolean = SingletonSpiel.paule.liegtEinKornAufDeinemFeld()
 
+/**
+ * Ist der Mund des Hamsters leer?
+ */
 fun istDeinMundLeer(): Boolean = SingletonSpiel.paule.istDeinMundLeer()
 
+/**
+ * Der Hamster "sagt" die übergebene [nachricht].
+ *
+ * Sie wird in der Spielkonsole ausgegeben.
+ */
 fun sage(nachricht: String) {
     SingletonSpiel.paule.sage(nachricht)
 }
