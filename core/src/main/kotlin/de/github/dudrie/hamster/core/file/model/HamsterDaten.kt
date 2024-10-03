5794bd18091733d95b8590569e598d0b7ce4d842
Dudrie <Dudrie@gmx.de>
2024-10-03 19:04:15 +0200

    Add comments to data classes for file content.

--- core/src/main/kotlin/de/github/dudrie/hamster/core/file/model/HamsterDaten.kt
+++ core/src/main/kotlin/de/github/dudrie/hamster/core/file/model/HamsterDaten.kt
@@ -5,6 +5,13 @@ import de.github.dudrie.hamster.core.model.util.Position
 import de.github.dudrie.hamster.core.model.util.Richtung
 import kotlinx.serialization.Serializable
 
+/**
+ * Daten eines [de.github.dudrie.hamster.core.model.hamster.InternerHamster] zur Speicherung in einer Datei.
+ *
+ * @param position [Position] des Hamsters.
+ * @param richtung [Richtung] des Hamsters.
+ * @param inventar Das Inventar des Hamsters.
+ */
 @Serializable
 data class HamsterDaten(
     val position: Position,
--- core/src/main/kotlin/de/github/dudrie/hamster/core/file/model/KachelDaten.kt
+++ core/src/main/kotlin/de/github/dudrie/hamster/core/file/model/KachelDaten.kt
@@ -4,5 +4,11 @@ import de.github.dudrie.hamster.core.model.kachel.Kachelinhalt
 import de.github.dudrie.hamster.core.model.util.Position
 import kotlinx.serialization.Serializable
 
+/**
+ * Daten einer [de.github.dudrie.hamster.core.model.kachel.Kachel] zum Speichern in einer Datei.
+ *
+ * @param position [Position] der Kachel.
+ * @param inhalt [Kachelinhalt] der Kachel.
+ */
 @Serializable
 data class KachelDaten(val position: Position, val inhalt: Kachelinhalt)
